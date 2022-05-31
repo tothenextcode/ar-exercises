f17a32ebb4912bf9aabcc7075f19d53401a9c9d9
JC Flores <tothenextcode@gmail.com>
2022-05-31 01:30:19 +0000

    Add relationship between store and employees, then add employees for store 1 and 2

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,8 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Francis", last_name: "Bourgouin", hourly_rate: 40)
+@store1.employees.create(first_name: "Andy", last_name: "Lindsay", hourly_rate: 45)
+@store2.employees.create(first_name: "Vasily", last_name: "Klimkin", hourly_rate: 35)
+@store2.employees.create(first_name: "David", last_name: "Halpin", hourly_rate: 30)
